1763ed1d3b87ab9dc40b05f05b80577c78d3e36d
Richard Remer <rich.remer@gmail.com>
2022-01-31 17:11:22 -0800

    public key authentication

--- src/sftp.js
+++ src/sftp.js
@@ -1,3 +1,5 @@
+import {timingSafeEqual} from "crypto";
+import ssh from "ssh2";
 import {FTPProtocol, hash} from "@zingle/sftpd";
 
 const {pbkdf2} = hash;
@@ -19,7 +21,7 @@ function authenticationListener({userdb}) {
     const user = await userdb.getItem(ctx.username);
 
     if (user?.hash) methods.push("password");
-    if (user?.key) methods.push("key");
+    if (user?.key) methods.push("publickey");
 
     if (methods.includes(ctx.method)) {
       console.info(`sftpd: authenticating with ${ctx.method} -- ${ctx.username}`);
@@ -35,9 +37,32 @@ function authenticationListener({userdb}) {
         } else {
           return ctx.reject(methods);
         }
+      case "publickey":
+        if (verifyKey(ctx, user)) {
+          client.username = ctx.username;
+          return ctx.accept();
+        } else {
+          return ctx.reject(methods);
+        }
     }
 
     return ctx.reject(methods);
+
+    function verifyKey(ctx, user) {
+      const key = ssh.utils.parseKey(user.key);
+
+      if (ctx.key.algo !== key.type) return false;
+      if (!check(ctx.key.data, key.getPublicSSH())) return false;
+      if (ctx.signature && key.verify(ctx.blob, ctx.signature) !== true) return false;
+
+      return true;
+    }
+
+    function check(input, allowed) {
+      const rejected = input.length !== allowed.length;
+      const match = timingSafeEqual(input, rejected ? input : allowed);
+      return match && !rejected;
+    }
   }
 }
 
